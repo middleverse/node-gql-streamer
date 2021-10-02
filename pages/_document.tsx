7333b8ade357efb18261826d46d57f61e883cd51
middleverse <arshi_b@hotmail.com>
2021-10-02 13:24:27 -0400

    pages: added server side rendering & styles to document

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,9 +1,25 @@
 import Document, { Head, Html, Main, NextScript } from "next/document";
+import { ServerStyleSheets } from "@material-ui/core";
+import React from "react";
 
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
+    const sheets = new ServerStyleSheets();
+    const originalRenderPage = ctx.renderPage; // reference to the render that happens on screen each time
+
+    ctx.renderPage = () =>
+      originalRenderPage({
+        enhanceApp: (App) => (props) => sheets.collect(<App {...props} />),
+      });
+
     const initialProps = await Document.getInitialProps(ctx);
-    return { ...initialProps };
+    return {
+      ...initialProps,
+      styles: [
+        ...React.Children.toArray(initialProps.styles),
+        sheets.getStyleElement(),
+      ],
+    };
   }
 
   render() {
