429c70ae9728db80e1a1ec39414e4baf727c18e1
middleverse <arshi_b@hotmail.com>
2021-10-02 18:27:32 -0400

    pages: added about page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,7 +5,10 @@ import { ThemeProvider } from "@material-ui/core";
 
 import { themeDark, themeLight } from "lib/theme";
 
+// NEXT JS uses _app component to initialize pages,
+// here we're overriding the default App component
 export default function MyApp({ Component, pageProps }) {
+  // @note wtf is useEffect??
   useEffect(() => {
     // Remove the server-side injected CSS
     const jssStyles = document.querySelector("#jss-server-side");
--- pages/about.tsx
+++ pages/about.tsx
@@ -2,16 +2,16 @@ import { Container, Typography, Box, Button } from "@material-ui/core";
 import Link from "next/link";
 import React from "react";
 
-export default function Index() {
+export default function About() {
   return (
     <Container maxWidth="sm">
       <Box my={4}>
         <Typography variant="h4" component="h1" gutterBottom>
           Next.js Example
         </Typography>
-        <Link href="/about">
+        <Link href="/">
           <Button variant="contained" color="primary">
-            Go to the about page
+            Go to the index page
           </Button>
         </Link>
       </Box>
